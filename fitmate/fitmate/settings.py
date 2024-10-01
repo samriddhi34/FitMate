14509d9c15593b521f8e2f5f2c7a873acbacded1
Samriddhi Sinha <samriddhisinha52@gmail.com>
2024-10-01 23:45:06 +0530

    Adding static directories

--- fitmate/fitmate/settings.py
+++ fitmate/fitmate/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'fit/static',  # Points to your app's static directory
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
